e8c4582ff9b6d8294190cbc20f85fc741f608afa
jayanth <jayanthchintuchintu@gmail.com>
2024-11-05 12:19:39 +0530

    cleanup

--- src/App.js
+++ src/App.js
@@ -4,20 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         Contact Book
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        Contact Book
+      </h1>
     </div>
   );
 }

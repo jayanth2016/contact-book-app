cccb22ee17df2c0de2f9f6cb961198889f94be1e
jayanth <jayanthchintuchintu@gmail.com>
2024-11-09 11:53:42 +0530

    commit new-one

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import React,{useState} from 'react'
 
 const App = () => {
-  const  [contacts, setContacts] = useState([
-    { id: 1, firstName: 'Jayanth', lastName: 'Kotapalli', email: 'jayanth@gmail.com' },
-    { id: 2, firstName: 'Rohan', lastName: 'Teja', email: 'rohan@gmail.com' },
-    { id: 3, firstName: 'Siva', lastName: 'Mani', email: 'sivagmail.com' },
+  const  [contact, setContacts] = useState([
+    { id: 1, firstName: 'Jayanth', lastName: 'Kotapalli', email: 'Jayanth@gmail.com' },
+    { id: 2, firstName: 'Rohan', lastName: 'Teja', email: 'Rohan@gmail.com' },
+    { id: 3, firstName: 'Siva', lastName: 'Mani', email: 'Siva@gmail.com' },
   ]);
 
   return (
     <div>
     <button>Create Contact</button>
     <ul>
-        {contacts.map((contact) => (
-          <p key={contact.id}>
-            <p><strong>firstName:</strong>{contact.firstName}, <strong>lastName: </strong>{contact.lastName},</p> 
-            <strong>Email:</strong> {contact.email}
-          </p>
-        ))}
-      </ul>
+      {contact.map ((contact) => (
+        <p key={contact.id}>
+        <p> <p><strong>firstName:</strong>{contact.firstName}, 
+              <strong>lastName: </strong>{contact.lastName},</p> 
+              <strong>Email:</strong> {contact.email}
+          </p>  
+        </p>
+      ))}
+    </ul>
     </div>
   )
 }

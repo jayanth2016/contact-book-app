9ea8e7f75ce6bfe0527a8eeb0869ac3de84ba97c
jayanth <jayanthchintuchintu@gmail.com>
2024-11-21 17:03:06 +0530

    commit-form change

--- src/App.js
+++ src/App.js
@@ -1,41 +1,70 @@
 import React, {useState}from 'react'
 
 const App = () => {
- const [contact,setTable]=useState([
-  {FirstName:'Jayanth', LastName: 'Reddy',  EmailAddress: 'Jayanthchintu@gmail.com'},
-  {FirstName:'Likith', LastName: 'Royal',  EmailAddress: 'Likith@gmail.com'},
-  {FirstName:'Fahad', LastName: 'shaik',  EmailAddress: 'Fahad@gmail.com'},
+ const [contacts,setContacts] = useState ()
+ const [showform,setShowForm] = useState (false)
+ const [contact,setContact]=useState([
+  {Name:'Swaroop',    surName: 'Reddy',   EmailAddress: 'swaroopchintu@gmail.com'},
+  {Name:'Dhanush',    surName: 'Royal',   EmailAddress: 'Dhanush@gmail.com'},
+  {Name:'Deekshith', surName: 'Sirish',  EmailAddress: 'Deekshith@gmail.com'},
  ])
 
   return (
-    <div>
-      <table border='1' style={{width:'100%,', textAlign:'left'}}>
+      <div>
+      <button onClick={()=> setShowForm(true)}>Add contact</button>
+      {!showform ? ( <table border='1' style={{width:'100%,', textAlign:'left'}}>                                                                                                                                                                                                                                                                  
       <tr>
+      <th>Serial Number</th>
       <th>FirstName</th>
       <th>LastName</th>
       <th>EmailAddress</th>
       </tr>
-      {contact.map((contact,name)=>(
-        <tr key={name}>
-        <td>{contact.FirstName}</td>
-        <td>{contact.LastName}</td>
+      {contact.map((contact,index)=>(
+        <tr key={index}>
+        <td>{index+1}</td>
+        <td>{contact.Name}</td>
+        <td>{contact.surName}</td>
         <td>{contact.EmailAddress}</td>
         </tr>
       ))}
-      </table>
+      </table>) : 
+     
       
+     <div>
 
+<input
+  type="text"
+  name="name"
+  placeholder="Name"
+  onChange={(e => setContacts({...contact,name : e.target.value}))} 
+
+/>
+<br />
+<input
+  type="text"
+  name="surname"
+  placeholder="Surname"
+  onChange={(e => setContacts({...contact,name : e.target.value}))} 
+
+/>
+<br />
+<input
+  type="email"
+  name="email"
+  placeholder="Email"
+  onChange={(e => setContacts({...contact,name : e.target.value}))} 
+
+/>
+<br />
+<button onClick={()=> setShowForm(false)}>Save Contact</button>
+</div>
+}
+    
     </div>
+
   )
 }
 
 export default App
-
-
-
-
-
-
-
 
 
